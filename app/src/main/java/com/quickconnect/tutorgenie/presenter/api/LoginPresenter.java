4d17576450043686b74f0ca6c62c04d573ad8f6e
rahulsale <rahulsale@yahoo.com>
2017-11-06 21:34:29 -0500

    Model to hold login information

---
+++ app/src/main/java/com/quickconnect/tutorgenie/presenter/api/LoginPresenter.java
@@ -0,0 +1,8 @@
+package com.quickconnect.tutorgenie.presenter.api;
+
+/**
+ * Created by rahulsale on 11/6/17.
+ */
+
+public interface LoginPresenter {
+}
---
+++ app/src/main/java/com/quickconnect/tutorgenie/view/impl/LoginViewImpl.java
@@ -0,0 +1,8 @@
+package com.quickconnect.tutorgenie.view.impl;
+
+/**
+ * Created by rahulsale on 11/6/17.
+ */
+
+public class LoginViewImpl {
+}
